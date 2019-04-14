22c8caefd6055886207f69b0a435f780e6cb395b
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-04-15 00:45:42 +0700

    Implement bot block chat response

--- web/js/main.js
+++ web/js/main.js
@@ -1,31 +1,45 @@
+let chatCounter = 0;
 let windowChat = document.getElementById("chat-container");
 let kotakPesan = document.getElementById("text-msg");
 let tombolKirim = document.getElementById("text-btn");
-
+let newline = document.createElement('br');
+let ERROR_MSG = "Maaf saya sedang offline.";
+let BOT_NAME = "Asti";
 
 tombolKirim.addEventListener('click', function (e) {
     e.preventDefault();
     let message = kotakPesan.value;
-    console.log(message);
-    createMessage(message);
-
+    processChat(message, ERROR_MSG); //sementara
 });
 
-function createMessage(message) {
+function myMessage(message) {
     let chat = document.createElement("span");
-    let br = document.createElement('br');
-    chat.classList.add('message', 'my-message');
+    chat.classList.add('message', 'my-message', 'chat', 'chat' + chatCounter.toString());
+    chatCounter++;
     chat.innerText = message;
-    console.log(chat);
-    windowChat.appendChild(chat);
-    windowChat.appendChild(br);
-
+    return chat;
 }
 
-// function raisechatMessages() {
-//     let messageList = document.querySelectorAll("#text-chat");
-// }
+function botMessage(message) {
+    let chat = document.createElement("span");
+    chat.classList.add('message', 'bot-message', 'chat', 'chat' + chatCounter.toString());
+    chatCounter++;
+    chat.innerHTML = `
+        <p class="bot-blockchat-name">${BOT_NAME}</p><br>${message}
+    `;
+    console.log(chat);
+    return chat;
+}
 
-// function raiseChat() {
+function processChat(myChat, botChat) {
+    myBlockChat = myMessage(myChat);
+    windowChat.appendChild(myBlockChat);
+    windowChat.appendChild(newline);
+    setTimeout(function () {
+        console.log(document.querySelector('.chat').getBoundingClientRect());
+    }, 1000);
+    botResponse = botMessage(botChat);
+    windowChat.appendChild(botResponse);
+    windowChat.appendChild(newline);
 
-// }
+}
