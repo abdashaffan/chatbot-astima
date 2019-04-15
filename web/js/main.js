dab441f848f8c2d2811e3ccdfe07c39b2a8c6f64
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-04-15 10:17:25 +0700

    Implement sticker chat feature (buat gantinya avatar)

--- web/js/main.js
+++ web/js/main.js
@@ -1,14 +1,15 @@
 let chatCounter = 0;
+let NUM_STICKERS = 11;
 let windowChat = document.getElementById("chat-content");
 let kotakPesan = document.getElementById("text-msg");
 let tombolKirim = document.getElementById("text-btn");
+let tombolStiker = document.getElementById("sticker");
 let newline = document.createElement('br');
 let ERROR_MSG = "Maaf saya sedang offline, silakan coba beberapa saat lagi. ";
 let BOT_NAME = "Astimah";
 
 
-tombolKirim.addEventListener('click', function (e) {
-    e.preventDefault();
+tombolKirim.addEventListener('click', function () {
     let message = kotakPesan.value;
     if (message !== "") {
         processChat(message, ERROR_MSG); //sementara
@@ -16,12 +17,39 @@ tombolKirim.addEventListener('click', function (e) {
     }
 });
 
+tombolStiker.addEventListener('click', processSticker);
+
+function mySticker() {
+    let sticker = document.createElement('img');
+    let num = Math.floor((Math.random() * NUM_STICKERS) + 1);
+    let src = `../web/assets/stickers/${num}.png`;
+    sticker.setAttribute('src', src);
+    chatCounter++;
+    sticker.classList.add('sticker', 'my-sticker', 'chat', 'chat' + chatCounter.toString());
+    return sticker;
+}
 
 function myMessage(message) {
     let chat = document.createElement("span");
     chatCounter++;
     chat.classList.add('message', 'my-message', 'chat', 'chat' + chatCounter.toString(), 'hidden');
     chat.innerText = message;
+    return chat;
+}
+
+function botSticker() {
+    let chat = document.createElement("span");
+    chatCounter++;
+    chat.classList.add('message', 'bot-message', 'chat', 'chat' + chatCounter.toString(), 'hidden');
+    chat.innerHTML = `
+        <p class="bot-blockchat-name">${BOT_NAME}</p><br>
+    `;
+    let sticker = document.createElement('img');
+    let num = Math.floor((Math.random() * NUM_STICKERS) + 1);
+    let src = `../web/assets/stickers/${num}.png`;
+    sticker.setAttribute('src', src);
+    sticker.classList.add('sticker', 'bot-sticker');
+    chat.appendChild(sticker);
     return chat;
 }
 
@@ -36,20 +64,10 @@ function botMessage(message) {
 }
 
 function raiseAllChatHistory(chatHistories, upscale) {
-
     for (let i = 0; i < chatHistories.length; i++) {
-
-
         let currentHeight = window.getComputedStyle(chatHistories[i]).bottom;
         currentHeight = Number(currentHeight.slice(0, -2));
-
-
-        let finalHeight = currentHeight + upscale;
-
-
         chatHistories[i].style.bottom = `${currentHeight + upscale + 10}px`;
-
-
     }
 
 }
@@ -75,6 +93,30 @@ function processChat(myChat, botChat) {
         raiseAllChatHistory(chatHistories, botChatUpInPixels);
         botResponse.classList.remove('hidden'); //Perlihatkan respon bot
 
-    }, 1000);
+    }, 500);
 
+}
+
+function processSticker() {
+    let initialChatHistories = document.querySelectorAll('.chat');
+    myStickerChat = mySticker();
+    windowChat.appendChild(myStickerChat);
+    if (chatCounter !== 1) {
+        let myCurrentChatClass = `.chat${chatCounter}`;
+        let myChatUpInPixels = document.querySelector(myCurrentChatClass).getBoundingClientRect().height;
+        raiseAllChatHistory(initialChatHistories, myChatUpInPixels);
+    }
+    myStickerChat.classList.remove('hidden');
+
+    let chatHistories = document.querySelectorAll('.chat'); //Masukkan pesan bot pada DOM secata hidden agar nilai height bisa diambil terlebih dahulu
+    botStickerChat = botSticker();
+    windowChat.appendChild(botStickerChat);
+    let botCurrentChatClass = `.chat${chatCounter}`;
+    setTimeout(function () {
+        //Angkat semua chat sebelum balasan bot ditampilkan (tidak di hidden)
+        let botChatUpInPixels = document.querySelector(botCurrentChatClass).getBoundingClientRect().height;
+        raiseAllChatHistory(chatHistories, botChatUpInPixels);
+        botStickerChat.classList.remove('hidden'); //Perlihatkan respon bot
+
+    }, 500);
 }
